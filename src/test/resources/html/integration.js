4eec9ce627a10265b7933a6d7ac4b8223a544129
Jeremy Landis <jeremylandis@hotmail.com>
2025-04-20 14:06:17 -0400

    Fix syntax error in javascript sample

--- src/test/resources/html/integration.js
+++ src/test/resources/html/integration.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2023 Alex Tunyk <alex at tunyk.com>.
+ * Copyright (c) 2011-2025 Alex Tunyk <alex at tunyk.com>.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,6 @@
  * regarding copyright ownership.
  */
 var myComplexObject = {
-	htmlTemplates: %s,
+	htmlTemplates: "%s",
 	anotherAttribute: "value"
 };
